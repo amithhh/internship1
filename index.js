9d41faf18b5773e5dc71ca686190347a97331b5e
Amith <amithp@Amiths-MacBook-Air.local>
2024-06-28 15:46:03 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,48 @@
+const express=require('express');
+const app=express();
+const cors=require('cors');
+const { MongoClient } = require('mongodb');
+app.use(cors())
+app.use(express.json())
+
+let user=[]
+app.get('/users',function(req,res){
+    res.json(user)
+})
+async function mongoConnect() {
+    let client = new MongoClient('mongodb+srv://anshif:nesRoWgW5SqAD0yF@cluster0.8dtglzr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
+    await client.connect();
+    db = client.db('jyothi');
+   ;
+ }
+ 
+app.get('/users', async function (req, res) {
+    let output = await db.collection('user').find({}).toArray();
+    res.json(output);
+});
+
+ 
+app.post('/Sign',function(req,res){
+    console.log(req.body)
+    user.push(req.body)
+    res.json(user)
+
+})
+app.post('/login',function(req,res){
+    console.log(req.body)
+    for (i=0;i<user.length;i++){
+        if(user[i].email==req.body.email){
+            if(user[i].pass1==req.body.pass1){
+                return res.json(user[i]);
+            }
+        }
+    }
+    return res.json("Email not found");
+})
+let s=50
+app.get('/s',function(req,res){
+    res.json(s)
+})
+app.listen(5001,function() {
+    console.log('Server ready listening on port 5001')
+})
